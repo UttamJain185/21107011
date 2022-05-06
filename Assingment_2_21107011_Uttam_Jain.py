c1c989e6812c516f2cf733e03e9ef7737e8815b7
UttamJain185 <104371490+UttamJain185@users.noreply.github.com>
2022-05-06 22:46:59 +0530

    Add files via upload

---
+++ Assingment_2_21107011_Uttam_Jain.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+#Question No.1 
+
+print("QUESTION No.1(a):")
+
+string_1=str("Python is a case sensitive language")
+#printing length of string_1
+print("length of string = ",len(string_1))
+
+print("QUESTION No.1(b):")
+
+#printing string_1 in reverse order
+print("String in reverse order : ",string_1[::-1])
+
+print("QUESTION No.1(c):")
+
+#slicing string_1
+string_2 = string_1[10:27]
+print("slice function result : ",string_2)
+
+print("QUESTION No.1(d):")
+
+#making change in string
+string_3=string_1[0:10] + "object oriented" + string_1[26:35]
+print("changing case sensitive to object orinted : ",string_3)
+
+print("QUESTION 1(e):")
+
+#printing index of "a" in string_1
+print("index of a : ",string_1.find("a"))
+
+print("QUESTION 1(f):")
+
+#removing white spaces from string_1
+print("after removing white spaces result : ",string_1.replace(" ",""))
+
+print("\n")
+
+# Question No.2
+print("QUESTION 2:")
+print("Enter your name:-")
+v1=input()
+print("Enter your SID")
+v2=input()
+print("Enter your department name")
+v3=input()
+print("Enter your CGPA")
+v4=input()
+#printing required details
+print("Hey",v1," Here!")
+print("My SID is", v2)
+print("I am from" ,v3, "department and my CGPA is" ,v4,)
+
+print("\n")
+
+# Question No.3
+
+print("QUESTION 3:")
+#initialising a and b
+a=56
+b=10
+
+print("QUESTION 3(a):")
+#printing a&b
+print("The value of a&b = ",a&b)
+
+print("QUESTION 3(b):")
+#printing a|b
+print("The value of a|b = ",a|b)
+
+print("QUESTION 3(c):")
+#printing a^b
+print("The value of a^b = ",a^b)
+
+print("QUESTION 3(d):")
+#left shift both a and b by 2 bits
+print("The value of a<<2 = ",a<<2,"\nThe value of b<<2 = ",b<<2)
+
+print("QUESTION 3(e):")
+#right shift a by 2  its and left shift b by 4 bits
+print("The value of a>>2 = ",a>>2,"\nThe value of b>>4 = ",b>>4)
+
+print("\n")
+
+#Question No.4
+
+print("QUESTION 4:")
+#to check if the word “name” is present in the string entered by the user
+print("Enter a sentence")
+sentence=str(input())
+if(sentence.find("name") >= 0):
+     print("true")
+else:
+    print("false")
+
+print("\n")
+
+# QUESTION NO.5
+print("QUESTION 5:")
+print("This program tell's that, the sides Entered by the user will form a triangle or not")
+#taking length of 3 sides as input from user
+print("Enter first side of triangle")
+side1=float(input())
+print("Enter second side of triangle")
+side2=float(input())
+print("Enter third side of triangle")
+side3=float(input())
+A=side1<side2+side3
+B=side2<side1+side3
+C=side3<side2+side1
+
+Result=str(A&B&C)
+print("The sides entered by the user can form a triangle?")
+Result=Result.replace("True","YES")
+Result=Result.replace("False","NO")
+print("The Answer is ",Result)
+print("\n")
+
+# QUESTION NO.6
+print("QUESTION 6:")
+#counting number of bits needed to be flipped to convert ‘a’ to ‘b’
+a=int(input("Enter the value of a:-"))
+b=int(input("Enter the value of b:-"))
+n = a^b
+count = 0
+while(n>0):
+        count += 1
+        n &= (n-1)
+print("number of bits flipped = ",count)
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
