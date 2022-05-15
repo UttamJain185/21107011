c1fe65c94607e25c6da883ff052bc30ce2adedf6
UttamJain185 <104371490+UttamJain185@users.noreply.github.com>
2022-05-15 10:14:58 +0530

    Add files via upload

---
+++ Assingnment_3_output_21107011.py
@@ -0,0 +1,94 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\AdminWS\.spyder-py3\Assingment_3_21107011_Uttam_Jain.py ==
+QUESTION NO.1
+Enter a number: 14
+Binary Equivalent is: 
+1 1 1 0 
+
+QUESTION NO.2
+enter your 1st number: 12
+enter your operator : *
+enter your 2nd number : 7
+12.0 * 7.0  =  84.0 
+
+QUESTION NO.3(a)
+result of the expression :  35 
+
+QUESTION NO.3(b)
+Enter your value of n: 12
+result of the expression :  66.0 
+
+QUESTION NO.3(c)
+Enter your value of r: 14.1
+result of the expression :  2498.32 
+
+QUESTION NO.3(d)
+Enter your value of r: 12
+Enter your value of a: 16
+Enter your value of b: 52
+result of the expression :  4.763039809569761 
+
+QUESTION N0.3(e)
+Enter your value of x1: 4
+Enter your value of x2: 2
+Enter your value of y1: 8
+Enter your value of y2: 4
+result of the expression :  2.0 
+
+QUESTION N0.4(a)
+0
+
+1
+
+2
+
+3
+
+4
+
+QUESTION N0.4(b)
+3
+
+4
+
+5
+
+6
+
+7
+
+8
+
+9
+
+QUESTION N0.4(c)
+4
+
+7
+
+10
+
+QUESTION N0.4(d)
+15
+
+13
+
+11
+
+9
+
+7
+
+QUESTION N0.4(e)
+
+
+QUESTION NO.5
+Enter number of hydrogen atoms : 
+3
+Enter number of Carbon atoms : 
+2
+Enter number of Oxygen atoms : 
+4
+The mass of molecule =  91.04282
