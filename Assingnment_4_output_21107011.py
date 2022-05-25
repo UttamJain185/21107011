bf45dc65c5aa7a2f38e0c0e89db3b4b9e607defe
UttamJain185 <104371490+UttamJain185@users.noreply.github.com>
2022-05-25 20:57:33 +0530

    Add files via upload

---
+++ Assingnment_4_output_21107011.py
@@ -0,0 +1,55 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\AdminWS\.spyder-py3\Assingment_4_21107011_Uttam_Jain.py ==
+QUESTION No.1:
+enter the number of marks: 67
+Grade of the student : B
+
+QUESTION No.2:
+enter the year: 2016
+It is a leap year
+
+QUESTION No.3:
+Question  1 :  2  x  19
+38
+Right!
+
+Question  2 :  3  x  15
+45
+Right!
+
+Question  3 :  4  x  10
+40
+Right!
+
+Question  4 :  3  x  10
+24
+Wrong. The answer is  30 
+
+Question  5 :  7  x  4
+32
+Wrong. The answer is  28 
+
+Question  6 :  7  x  17
+119
+Right!
+
+Question  7 :  4  x  1
+4
+Right!
+
+Question  8 :  10  x  20
+200
+Right!
+
+Question  9 :  2  x  20
+34
+Wrong. The answer is  40 
+
+Question  10 :  6  x  12
+45
+Wrong. The answer is  72 
+
+QUESTION No.4:
+There are  177  candies in the jar
